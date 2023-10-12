df140a37acc1aba3f6ff45a8951be5d149e3d0b2
LucRademakers <LucRademakers99@gmail.com>
2023-10-12 14:46:57 +0200

    some init msg

---
+++ src/test/java/nl/zuyd/ec/NativeBearerTokenAuthenticationIT.java
@@ -0,0 +1,7 @@
+package nl.zuyd.ec;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class NativeBearerTokenAuthenticationIT extends BearerTokenAuthenticationTest {
+}
